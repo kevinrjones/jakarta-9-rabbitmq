e90f8180edd90cac95c9e3589dc0cd33392c2020
Kevin Jones <kevin@knowledgespike.com>
2022-09-27 08:26:43 +0100

    Made code simpler

--- checkpoints/m5/rpc/src/main/java/com/knowledgespike/Consumer.java
+++ checkpoints/m5/rpc/src/main/java/com/knowledgespike/Consumer.java
@@ -16,12 +16,10 @@ public class Consumer {
     }
 
     public void sendResponseMessage(String replyTo, String correlationId, long deliveryTag) throws IOException, TimeoutException {
-//        AMQP.BasicProperties props = new AMQP.BasicProperties.Builder()
-//                .correlationId(correlationId)
-//                .build();
+        AMQP.BasicProperties props = null;
 
         System.out.println(" [x] About to reply");
-        channel.basicPublish("", replyTo, null, "reply".getBytes(StandardCharsets.UTF_8));
+        channel.basicPublish("", replyTo, props, "reply".getBytes(StandardCharsets.UTF_8));
         channel.basicAck(deliveryTag, false);
     }
 }

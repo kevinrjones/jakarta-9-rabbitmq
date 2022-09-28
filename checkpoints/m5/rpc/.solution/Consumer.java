af76f4fd47df0a04672a188c14c82a7c01fa7b2f
Kevin Jones <kevin@knowledgespike.com>
2022-09-28 13:14:26 +0100

    Added solution

---
+++ checkpoints/m5/rpc/.solution/Consumer.java
@@ -0,0 +1,28 @@
+package com.knowledgespike;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.impl.AMQBasicProperties;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+
+    private Channel channel;
+
+
+    public Consumer(Channel channel) {
+        this.channel = channel;
+    }
+
+    public void sendResponseMessage(String replyTo, String correlationId, long deliveryTag) throws IOException, TimeoutException {
+        AMQP.BasicProperties props = new AMQP.BasicProperties.Builder()
+                .correlationId(correlationId)
+                .build();
+
+        System.out.println(" [x] About to reply");
+        channel.basicPublish("", replyTo, props, "reply".getBytes(StandardCharsets.UTF_8));
+        channel.basicAck(deliveryTag, false);
+    }
+}

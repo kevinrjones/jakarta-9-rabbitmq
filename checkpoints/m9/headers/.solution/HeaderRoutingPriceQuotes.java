8532be38340cef4460db000069e628af7a9fb2b0
Kevin Jones <kevin@knowledgespike.com>
2022-09-28 13:37:04 +0100

    Added solution

---
+++ checkpoints/m9/headers/.solution/HeaderRoutingPriceQuotes.java
@@ -0,0 +1,30 @@
+package com.knowledgespike;
+
+import com.rabbitmq.client.AMQP;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+public class HeaderRoutingPriceQuotes {
+
+    private Channel channel;
+
+
+    public HeaderRoutingPriceQuotes(Channel channel) {
+        this.channel = channel;
+    }
+
+    public boolean sendMessage(String message, Map<String, Object> headers) {
+        try {
+            AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                    .headers(headers)
+                    .build();
+            channel.basicPublish("headers-exchange", "", properties, message.getBytes());
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+}
